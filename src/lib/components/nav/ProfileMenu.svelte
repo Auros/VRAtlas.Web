e4aff6af138eff26dc5dafa0fa0282b85412e423
Auros Nexus <41306347+Auros@users.noreply.github.com>
2022-10-26 03:44:46 -0400

    Profile Menu Dark Mode

--- src/lib/components/nav/ProfileMenu.svelte
+++ src/lib/components/nav/ProfileMenu.svelte
@@ -2,7 +2,8 @@
     role="menu"
     aria-orientation="vertical"
     aria-labelledby="user-menu-button"
-    class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+    class="absolute right-0 z-10 mt-3 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none
+    dark:bg-neutral-800 dark:shadow-neutral-800"
     tabindex="-1">
     <slot />
 </div>
