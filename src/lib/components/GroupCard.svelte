8587fed9f3e09c3bc1e8038391504581fd6470b8
Auros Nexus <41306347+Auros@users.noreply.github.com>
2023-02-23 07:42:13 -0500

    Cleanup Group Icon for mobile devices

--- src/lib/components/GroupCard.svelte
+++ src/lib/components/GroupCard.svelte
@@ -9,8 +9,8 @@
 </script>
 
 <a href={clickable ? `/groups/${group.id}` : undefined} class="card card-hover variant-glass-surface overflow-hidden">
-    <header>
-        <Avatar src={picture(group.icon)} width="2xl:w-16 xl:w-14 lg:w-16 md:w-24 w-16" class="absolute top-2 left-2" />
+    <header class="relative">
+        <Avatar src={picture(group.icon)} width="2xl:w-16 xl:w-14 lg:w-16 md:w-24 w-16" class="absolute top-2 left-2 hidden xl:flex" />
         <img src={picture(group.banner)} alt={`${group.name}'s Banner'`} class="select-none object-cover bg-black/50 w-full aspect-[24/4]" />
     </header>
     <div class="p-4">
