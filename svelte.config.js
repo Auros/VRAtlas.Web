e9e3abaf7292d57d7a713f92e9176a47274609d8
Auros Nexus <41306347+Auros@users.noreply.github.com>
2023-02-23 18:58:10 -0500

    Revert "Added Vercel Adapter"
    This reverts commit 3cd4d9cb55efe25d2129141cbfe7ff140d55d0e7.

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,22 @@
+<<<<<<< HEAD
+import adapter from '@sveltejs/adapter-auto';
+=======
 import node from '@sveltejs/adapter-node';
 import vercel from '@sveltejs/adapter-vercel';
+>>>>>>> parent of de30857 (Update svelte.config.js)
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
+<<<<<<< HEAD
+        adapter: adapter(),
+=======
         adapter: [
             vercel({ regions: ['sfo1'] }),
             node()
         ],
+>>>>>>> parent of de30857 (Update svelte.config.js)
         csrf: {
             checkOrigin: process.env.MODE !== 'dev'
         }
