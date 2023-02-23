de308579440939c155c013502b05885c8e8b1860
Auros Nexus <41306347+Auros@users.noreply.github.com>
2023-02-23 18:06:12 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,10 @@
-import node from '@sveltejs/adapter-node';
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-        adapter: [
-            vercel({ regions: ['sfo1'] }),
-            node()
-        ],
+        adapter: adapter({ regions: ['sfo1'] }),
         csrf: {
             checkOrigin: process.env.MODE !== 'dev'
         }
