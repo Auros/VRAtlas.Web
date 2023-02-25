5199bde28b3a37d70db4fbb26ba2512c6b4ba008
Auros Nexus <41306347+Auros@users.noreply.github.com>
2023-02-24 23:07:32 -0500

    Made login flow more robust

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,63 @@
-import type { Handle } from '@sveltejs/kit';
+import { api } from '$lib';
+import { PUBLIC_OAUTH_URL } from '$env/static/public';
+import { error, type Handle, type RequestEvent } from '@sveltejs/kit';
+
+type Tokens = {
+    accessToken: string;
+    expires_in: number;
+};
 
 export const handle = (async ({ event, resolve }) => {
+    const { url, locals, cookies } = event;
+    // If we're in the auth flow, validate the user here.
+    if (url.pathname.endsWith('login/callback')) {
+        await loginUser(event);
+    }
+
     // Supply the local user's token into the event locals.
-    event.locals.token = event.cookies.get('token');
+    locals.token = cookies.get('token');
 
     // Handle the request.
     return await resolve(event);
+
 }) satisfies Handle;
+
+const loginUser = async (event: RequestEvent) => {
+    const { url, locals, cookies } = event;
+    const code = url.searchParams.get('code');
+    const challenge = cookies.get('challenge');
+    const authUrl = new URL(PUBLIC_OAUTH_URL);
+
+    if (!(code && challenge)) {
+        throw error(401, 'Authentication failed, missing parameters');
+    }
+
+    // If the provided challenge state does not equal the one we generated, throw.
+    // (Preventing CSRF Attacks)
+    const state = url.searchParams.get('state');
+    if (state !== challenge) {
+        console.log('Non-matching OAuth challenge detected.');
+        throw error(401, 'Non-matching OAuth challenge. Cannot verify authorization state.');
+    }
+
+    console.log('Deleting challenge cookie')
+    // Delete the challenge cookie as it's no longer necessary
+    cookies.delete('challenge', { path: '/' });
+
+    // Setup the url for getting our tokens
+    const params = new URLSearchParams();
+    params.set('redirectUri', authUrl.searchParams.get('redirect_uri') ?? 'MISCONFIGURED_WEBSITE');
+    params.set('code', code);
+
+    console.log('Acquiring access token');
+    // Fetch the access token response.
+    const tokens = await api.get<Tokens>(`/auth/token?${params.toString()}`, fetch);
+
+    console.log('Setting token cookie');
+    // Set the token in the cookies and locals
+    locals.token = tokens.accessToken;
+    cookies.set('token', tokens.accessToken, {
+        maxAge: tokens.expires_in,
+        path: '/'
+    });
+}
--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,69 +1,23 @@
-import api from '$lib/api';
-import type { Actions, PageServerLoad } from './$types';
-import { error, redirect } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
 import { PUBLIC_OAUTH_URL } from '$env/static/public';
+import type { Actions, PageServerLoad } from './$types';
 
-type Tokens = {
-    accessToken: string;
-    expires_in: number;
-};
-
-export const load = (async ({ cookies, url, fetch, locals }) => {
-    console.log('Starting login request');
-    let challenge = cookies.get('challenge');
-    const code = url.searchParams.get('code');
+export const load = (async ({ cookies }) => {
+    console.log('Starting login process');
     const authUrl = new URL(PUBLIC_OAUTH_URL);
 
-    // If there's no challenge or there's no code, it means we need to log in the user.
-    console.log(challenge);
-    if (!challenge || !code) {
-        console.log('Challenging user login request.');
+    // Setup our auth state and url
+    const challenge = crypto.randomUUID();
+    console.log(`Challenging user login request: (${challenge})`);
 
-        // Setup our auth state and url
-        challenge = crypto.randomUUID();
-
-        authUrl.searchParams.set('state', challenge);
-        cookies.set('challenge', challenge);
-
-        // Redirect to oauth page
-        throw redirect(307, authUrl.toString());
-    }
-
-    console.log('Processing challenge and code');
-
-    // If the provided challenge state does not equal the one we generated, throw.
-    // (Preventing CSRF Attacks)
-    const state = url.searchParams.get('state');
-    if (state !== challenge) {
-        console.log('Non-matching OAuth challenge detected.');
-        throw error(401, 'Non-matching OAuth challenge. Cannot verify authorization state.');
-    }
-
-    console.log('Deleting challenge cookie')
-    // Delete the challenge cookie as it's no longer necessary
-    cookies.delete('challenge');
-
-    // Setup the url for getting our tokens
-    const params = new URLSearchParams();
-    params.set('redirectUri', authUrl.searchParams.get('redirect_uri') ?? 'MISCONFIGURED_WEBSITE');
-    params.set('code', code);
-
-    console.log('Acquiring access token');
-    // Fetch the access token response.
-    const tokens = await api.get<Tokens>(`/auth/token?${params.toString()}`, fetch);
-
-    console.log('Setting token cookie');
-    // Set the token in the cookies and locals
-    locals.token = tokens.accessToken;
-    cookies.set('token', tokens.accessToken, {
-        maxAge: tokens.expires_in,
+    authUrl.searchParams.set('state', challenge);
+    cookies.set('challenge', challenge, {
+        maxAge: 60 * 10, // 10 minutes 
         path: '/'
     });
 
-    console.log('Successfully logged in user.')
-    return {
-        token: tokens.accessToken
-    };
+    // Redirect to oauth page
+    throw redirect(307, authUrl.toString());
 }) satisfies PageServerLoad;
 
 export const actions = {
