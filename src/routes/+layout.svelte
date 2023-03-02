c22361233d033232e57bde456c52d45a8a8e2540
Auros Nexus <41306347+Auros@users.noreply.github.com>
2023-03-02 08:58:13 -0500

    Branding update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,6 +14,7 @@
     import { navigating, page } from '$app/stores';
     import type { Notification } from '$lib/types';
     import { notificationStore } from '$lib/stores';
+    import banner from '$lib/images/banner-border.webp';
     import { PUBLIC_API_URL } from '$env/static/public';
     import { HubConnectionBuilder } from '@microsoft/signalr';
     import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
@@ -73,9 +74,7 @@
             <svelte:fragment slot="lead">
                 <div class="flex flex-row md:gap-6 gap-3 items-center">
                     <a href="/">
-                        <div class="text-2xl select-none">
-                            <b>VR Atlas</b>
-                        </div>
+                        <img src={banner} class="h-8" alt="Banner">
                     </a>
                     <span class="divider-vertical h-10" />
                     <a href="/" class="hidden md:flex">
