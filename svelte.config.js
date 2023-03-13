2c466ad0ec1ab6c52bad74f1e7e9bd9bf0f60b11
Auros Nexus <41306347+Auros@users.noreply.github.com>
2023-03-12 20:10:15 -0400

    Use the edge runtime

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,10 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-        adapter: adapter({ regions: ['sfo1'] }),
+        adapter: adapter({
+            regions: ['sfo1'],
+            runtime: 'edge'
+        }),
         csrf: {
             checkOrigin: process.env.MODE !== 'dev'
         }
