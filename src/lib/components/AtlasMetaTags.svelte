3ff6d430c0a40ac429ba1ffa7448bb0faacf79df
Auros Nexus <41306347+Auros@users.noreply.github.com>
2023-03-21 18:14:29 -0400

    Added default logo when embedding

--- src/lib/components/AtlasMetaTags.svelte
+++ src/lib/components/AtlasMetaTags.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
-    import { PUBLIC_SERVING_URL } from '$env/static/public';
     import { MetaTags } from 'svelte-meta-tags';
+    import { PUBLIC_SERVING_URL } from '$env/static/public';
 
     export let title: string;
     export let description: string | undefined = undefined;
     export let url: string | undefined = undefined;
     export let image: string | undefined = undefined;
     export let twitter = false;
+    export let large = true;
 </script>
 
 <MetaTags
@@ -14,16 +15,13 @@
     titleTemplate="%s • VR Atlas"
     {description}
     twitter={twitter && image ? {
-        cardType: 'summary_large_image',
-        description,
-        title,
-        image
+        cardType: large ? 'summary_large_image' : 'summary'
     } : undefined}
     openGraph={{
         type: 'website',
         title,
         description,
         url: url ? `${PUBLIC_SERVING_URL}${url}` : undefined,
-        images: image ? [{ url: image }] : undefined
+        images: [{ url: image ?? `${PUBLIC_SERVING_URL}/icon.png` }]
     }}
 />
