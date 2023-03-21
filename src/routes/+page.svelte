c59f39b89bcd6244d6d1e3cd4a1fec9881bb59c0
Auros Nexus <41306347+Auros@users.noreply.github.com>
2023-03-21 18:33:41 -0400

    Remove "occurring" from website description.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
     export let data: PageData;
 </script>
 
-<AtlasMetaTags title={`Home`} description={`The open source hub for finding events occurring in virtual reality.`} />
+<AtlasMetaTags title={`Home`} description={`The open source hub for finding events in virtual reality.`} />
 
 <div class="overflow-hidden lg:h-[480px] h-64 lg:bg-[center_top_-14rem] bg-cover" style={`background-image: url(${hero})`}>
     <div class="backdrop-blur-lg w-full h-full grid place-items-center text-center">
@@ -14,7 +14,7 @@
             <h1 class="scale-150 bg-gradient-to-br from-purple-300 via-purple-600 to-purple-900 bg-clip-text text-transparent box-decoration-clone">
                 <b>VR Atlas</b>
             </h1>
-            <p>The open source hub for finding events occurring in virtual reality.</p>
+            <p>The open source hub for finding events in virtual reality.</p>
         </div>
     </div>
 </div>
