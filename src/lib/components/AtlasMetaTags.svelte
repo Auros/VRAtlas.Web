85aaea26d5beb63b7bd639b38d61ae3ce34bfa19
Auros Nexus <41306347+Auros@users.noreply.github.com>
2023-03-21 18:30:34 -0400

    Strip markdown from embed links

--- src/lib/components/AtlasMetaTags.svelte
+++ src/lib/components/AtlasMetaTags.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { MetaTags } from 'svelte-meta-tags';
+    import markdownToTxt from 'markdown-to-txt';
     import { PUBLIC_SERVING_URL } from '$env/static/public';
 
     export let title: string;
@@ -13,15 +14,15 @@
 <MetaTags
     {title}
     titleTemplate="%s • VR Atlas"
-    {description}
+    description={description ? markdownToTxt(description) : undefined}
     twitter={twitter && image ? {
         cardType: large ? 'summary_large_image' : 'summary'
     } : undefined}
     openGraph={{
         type: 'website',
         title,
-        description,
         url: url ? `${PUBLIC_SERVING_URL}${url}` : undefined,
-        images: [{ url: image ?? `${PUBLIC_SERVING_URL}/icon.png` }]
+        images: [{ url: image ?? `${PUBLIC_SERVING_URL}/icon.png` }],
+        description: description ? markdownToTxt(description) : undefined,
     }}
 />
